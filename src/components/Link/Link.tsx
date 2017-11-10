8f7317fd259f9dec0a4cb16d6a73aca795a5ee22
Danny Eck <daniel.eck@accenture.com>
2017-11-10 13:48:44 -0800

    Extensible classNames for Link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
 import { StatelessComponent } from 'react';
 import * as PropTypes from 'prop-types';
+import * as classnames from 'classnames';
 
 export interface LinkProps {
   href?: string;
   className?: string;
 }
 
-const Link: StatelessComponent<LinkProps> = ({ href, children }) => (
-  <a href={href}>
+const Link: StatelessComponent<LinkProps> = ({ href, children, className }) => (
+  <a
+    href={href}
+    className={classnames('cwds-link', className)}
+  >
     {children}
   </a>
 );
 
 Link.propTypes = {
   href: PropTypes.string,
+  className: PropTypes.string,
 };
 
 Link.defaultProps = {
