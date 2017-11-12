a18b8dbb66e22fc6d8f4743bfda647022b3aba6d
Danny Eck <daniel.eck@accenture.com>
2017-11-11 20:18:20 -0800

    WiP - Start Row and Column components

---
+++ src/components/Col/Col.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { StatelessComponent } from 'react';
+import * as PropTypes from 'prop-types';
+
+export interface ColProps {
+
+}
+
+const propTypes = {
+
+};
+
+const defaultProps = {
+
+};
+
+const Col: StatelessComponent<ColProps> = ({
+
+}) => (
+  <div>COL</div>
+);
+
+Col.propTypes = propTypes;
+Col.defaultProps = defaultProps;
+
+export default Col;
---
+++ src/components/Row/Row.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { StatelessComponent } from 'react';
+import * as PropTypes from 'prop-types';
+
+export interface RowProps {
+
+}
+
+const propTypes = {
+
+};
+
+const defaultProps = {
+
+};
+
+const Row: StatelessComponent<RowProps> = ({
+
+}) => (
+  <div>ROW</div>
+);
+
+Row.propTypes = propTypes;
+Row.defaultProps = defaultProps;
+
+export default Row;
