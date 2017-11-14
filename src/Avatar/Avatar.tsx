7dcb1a2f228b8b9972019c1c88283c53d9d47636
Danny Eck <daniel.eck@accenture.com>
2017-11-14 09:38:54 -0800

    Add component Avatar

--- src/Avatar/Avatar.tsx
+++ src/Avatar/Avatar.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
 import * as PropTypes from 'prop-types';
+import Icon from '../Icon';
 
-export interface AvatarProps {}
+export interface AvatarProps {
+  imgUrl: string;
+}
 
-const propTypes = {};
+const propTypes = {
+  imgUrl: PropTypes.string,
+};
 
-const defaultProps = {};
+const defaultProps = {
 
-const Avatar: React.SFC<AvatarProps> = ({ }) => (
-  <div>AVATAR</div>
+};
+
+const mkAvatar = (imgUrl: string): React.ReactElement<{}> => {
+  return imgUrl
+    ? <img className="avatar-img rounded-circle" src={imgUrl} />
+    : <Icon className="avatar-icon" name="user-circle" size="4x" />
+    ;
+};
+
+const Avatar: React.SFC<AvatarProps> = ({
+  imgUrl
+}) => (
+  <div className="avatar">
+    {mkAvatar(imgUrl)}
+  </div>
 );
 
 Avatar.propTypes = propTypes;
