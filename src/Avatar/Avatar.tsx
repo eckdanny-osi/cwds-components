125b5143b6900e6ed9f92aa2f5642a23fe0870ab
Danny Eck <daniel.eck@accenture.com>
2017-11-14 19:54:28 -0800

    Fix Avatar component

--- src/Avatar/Avatar.tsx
+++ src/Avatar/Avatar.tsx
@@ -5,30 +5,60 @@ import Icon from '../Icon';
 
 export interface AvatarProps {
   imgUrl: string;
+  size: string;
 }
 
 const propTypes = {
   imgUrl: PropTypes.string,
+  size: PropTypes.oneOf(['sm', 'lg']),
 };
 
 const defaultProps = {
 
 };
 
-const mkAvatar = (imgUrl: string): React.ReactElement<{}> => {
-  return imgUrl
-    ? <img className="avatar-img rounded-circle" src={imgUrl} />
-    : <Icon className="avatar-icon" name="user-circle" size="4x" />
-    ;
-};
+// const mkAvatar = (size: string, imgUrl: string): React.ReactElement<AvatarProps> => {
+//   return imgUrl
+//     ? <img
+//         className={classNames(
+//           'avatar-img',
+//           'rounded-circle',
+//           size && `avatar-${size}`,
+//         )}
+//         src={imgUrl}
+//       />
+//     : <Icon
+//         className={classNames(
+//           'avatar-icon',
+//           size && `avatar-${size}`,
+//         )}
+//         name="user-times"
+//         size="4x"
+//       />
+//     ;
+// };
+
+// const addClasses = (Component: any, ...classes: string[]) => <Component className={classNames(classes)} />;
+
+const AvatarImg: React.SFC<{ imgUrl: string, classes: string[] }> = ({
+  imgUrl,
+  classes,
+}) => <img src={imgUrl} className={classNames(classes)} />;
+
+const AvatarBlank: React.SFC<{ classes: any }> = (
+  classes,
+) => <Icon name="user-circle" size="4x" className={classNames(classes)} />;
 
 const Avatar: React.SFC<AvatarProps> = ({
-  imgUrl
-}) => (
-  <div className="avatar">
-    {mkAvatar(imgUrl)}
-  </div>
-);
+  imgUrl,
+  size
+}) => {
+  const classes = classNames('avatar', 'rounded-circle', size && `avatar-${size}`);
+  return imgUrl
+    ? <img src={imgUrl} className={classes} />
+    : <Icon name="user" size="3x" className={classes} />
+    ;
+}
 
 Avatar.propTypes = propTypes;
 Avatar.defaultProps = defaultProps;
